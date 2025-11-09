8132126fd6396f3caa6e3bc5a9f46f975d8db112
Keanu-Weeves <edwards.cody51@yahoo.com>
2025-11-08 22:09:15 -0500

    added AI project to projects

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -6,15 +6,15 @@ import Card from "./Card";
 const projects = [
   {
     title: "Little Lemon Restaurant app",
-    description:"WORK IN PROGRESS! Featuring a rotating carousel and a reserve table form with a pop-up modal. View live now and follow for updates!",
+    description:"Featuring a rotating carousel and a reserve table form with a pop-up modal. View live now and follow for updates!",
     url: 'https://littlelemonreactapp.netlify.app/',
     getImageSrc: () => require("../images/LittleLemonThumb.png"),
   },
   {
-    title: "Coming Soon",
-    description:"",
-    url: null,
-    getImageSrc: () => require("../images/coming-soon.jpg"),
+    title: "brAIn-e  (AI Educational Tutor) ",
+    description:"Meet brAIn-e! A work in progress AI - private personal project so details are under wraps, but follow along!",
+    url: 'https://aiedututor.netlify.app/',
+    getImageSrc: () => require("../images/brAIn-e-logo.png"),
   },
   {
     title: "Coming Soon",
